87757ec48d01b44370abd846cd9a1a69a3926066
romansavcak <savchak181998@gmail.com>
2023-01-19 23:32:16 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import {Simpsons} from "./components/Simpsons/Simpsons";
-import {RickAndMortys} from "./components/RickAndMortys/RickAndMortys";
 
 function App() {
   return (
     <div className="App">
-        {/*<Simpsons/>*/}
-        <RickAndMortys/>
+
     </div>
   );
 }
