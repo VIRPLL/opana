2f644e6c742efba1f1d656d52df7da37fbc4e917
User <savchak181998@gmail.com>
2023-01-29 10:53:17 +0200

    я

---
+++ src/config/urls.js
@@ -0,0 +1,8 @@
+export const baseURL = 'https://jsonplaceholder.typicode.com/';
+
+export const urls = {
+    todos: '/todos',
+    albums: '/albums',
+    comments: '/comments',
+    postById: (id) => `/post/${id}`
+}
